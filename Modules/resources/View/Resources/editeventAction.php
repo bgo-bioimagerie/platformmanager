8cb031c7a7304aa8c0bbdc7bdeab00e03633e2b8
Sylvain Prigent <sylvain.prigent@univ-rennes1.fr>
2016-12-12 13:43:44 +0100

    fix a view bug

--- Modules/resources/View/Resources/editeventAction.php
+++ Modules/resources/View/Resources/editeventAction.php
@@ -3,18 +3,18 @@
 <!-- body -->     
 <?php startblock('content') ?>
     
-    <div class="col-md-12" id="pm-content">
+    <div class="col-md-10" id="pm-content">
 
     <?php include "Modules/resources/View/Resources/edittabs.php" ?>
     <div class="col-xs-12"><p></p></div>
     
     
-    <div class="col-xs-12 col-md-7 col-md-offset-1" id="pm-form">
+    <div class="col-xs-12 col-md-7" id="pm-form">
         <?php echo $formEvent ?>
     </div>
     
     <?php if ($id_event > 0){ ?>
-    <div class="col-xs-12 col-md-4">
+    <div class="col-xs-12 col-md-5">
         <div class="col-xs-12" id="pm-form">
             <?php echo $formDownload ?>
         </div>
