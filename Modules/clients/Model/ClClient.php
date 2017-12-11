d6b3af6556ae98a9dc60d8aaf9a64d4121a745cc
Sylvain Prigent <meriadec.prigent@gmail.com>
2017-12-11 10:57:42 +0100

    update client module with multiple address

--- Modules/clients/Model/ClClient.php
+++ Modules/clients/Model/ClClient.php
@@ -11,20 +11,26 @@ class ClClient extends Model {
         $this->setColumnsInfo("id_space", "int(11)", 0);
         $this->setColumnsInfo("name", "varchar(255)", "");
         $this->setColumnsInfo("contact_name", "varchar(255)", "");
-        $this->setColumnsInfo("institution", "varchar(255)", "");
-        $this->setColumnsInfo("building_floor", "varchar(255)", "");
-        $this->setColumnsInfo("service", "varchar(255)", "");
-        $this->setColumnsInfo("address", "text", "");
-        $this->setColumnsInfo("zip_code", "varchar(20)", "");
-        $this->setColumnsInfo("city", "varchar(255)", "");
-        $this->setColumnsInfo("country", "varchar(255)", "");
+        $this->setColumnsInfo("address_delivery", "int(11)", 0);
+        $this->setColumnsInfo("address_invoice", "int(11)", 0);
         $this->setColumnsInfo("phone", "varchar(20)", "");
         $this->setColumnsInfo("email", "varchar(255)", "");
         $this->setColumnsInfo("pricing", "int(11)", "");
+        $this->setColumnsInfo("invoice_send_preference", "int(11)", 0); // 1 email; 2 postal
         $this->primaryKey = "id";
     }
-    
-    public function getPricingID($id){
+
+    public function setAddressDelivery($id, $id_addressdelivery) {
+        $sql = "UPDATE cl_clients SET address_delivery=? WHERE id=?";
+        $this->runRequest($sql, array($id_addressdelivery, $id));
+    }
+
+    public function setAddressInvoice($id, $id_addressinvoice) {
+        $sql = "UPDATE cl_clients SET address_invoice=? WHERE id=?";
+        $this->runRequest($sql, array($id_addressinvoice, $id));
+    }
+
+    public function getPricingID($id) {
         $sql = "SELECT pricing FROM cl_clients WHERE id=?";
         $tmp = $this->runRequest($sql, array($id))->fetch();
         return $tmp[0];
@@ -40,14 +46,14 @@ class ClClient extends Model {
         }
         return $data;
     }
-    
-    public function getName($id){
+
+    public function getName($id) {
         $sql = "SELECT name FROM cl_clients WHERE id=?";
         $data = $this->runRequest($sql, array($id))->fetch();
         return $data[0];
     }
-    
-    public function getIdFromName($name){
+
+    public function getIdFromName($name) {
         $sql = "SELECT id FROM cl_clients WHERE name=?";
         $data = $this->runRequest($sql, array($name))->fetch();
         return $data[0];
@@ -71,16 +77,12 @@ class ClClient extends Model {
                 "id" => 0,
                 "name" => "",
                 "contact_name" => "",
-                "institution" => "",
-                "building_floor" => "",
-                "service" => "",
-                "address" => "",
-                "zip_code" => "",
-                "city" => "",
-                "country" => "",
+                "address_delivery" => 0,
+                "address_invoice" => 0,
                 "phone" => "",
                 "email" => "",
-                "pricing" => 0
+                "pricing" => 0,
+                "invoice_send_preference" => 1
             );
         }
 
@@ -88,18 +90,14 @@ class ClClient extends Model {
         return $this->runRequest($sql, array($id))->fetch();
     }
 
-    public function set($id, $id_space, $name, $contact_name, $institution, $building_floor, $service, $address, $zip_code, $city, $country, $phone, $email, $pricing) {
+    public function set($id, $id_space, $name, $contact_name, $phone, $email, $pricing, $invoice_send_preference) {
         if ($id == 0) {
-            $sql = 'INSERT INTO cl_clients (id_space, name, contact_name, institution, building_floor, service, address, 
-            zip_code, city, country, phone, email, pricing) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)';
-            $this->runRequest($sql, array($id_space, $name, $contact_name, $institution, $building_floor, $service, $address,
-                $zip_code, $city, $country, $phone, $email, $pricing));
+            $sql = 'INSERT INTO cl_clients (id_space, name, contact_name, phone, email, pricing, invoice_send_preference) VALUES (?,?,?,?,?,?,?)';
+            $this->runRequest($sql, array($id_space, $name, $contact_name, $phone, $email, $pricing, $invoice_send_preference));
             return $this->getDatabase()->lastInsertId();
         } else {
-            $sql = 'UPDATE cl_clients SET id_space=?, name=?, contact_name=?, institution=?, building_floor=?, service=?, address=?, 
-            zip_code=?, city=?, country=?, phone=?, email=?, pricing=? WHERE id=?';
-            $this->runRequest($sql, array($id_space, $name, $contact_name, $institution, $building_floor, $service, $address,
-                $zip_code, $city, $country, $phone, $email, $pricing, $id));
+            $sql = 'UPDATE cl_clients SET id_space=?, name=?, contact_name=?, phone=?, email=?, pricing=?, invoice_send_preference=? WHERE id=?';
+            $this->runRequest($sql, array($id_space, $name, $contact_name, $phone, $email, $pricing, $invoice_send_preference, $id));
             return $id;
         }
     }
