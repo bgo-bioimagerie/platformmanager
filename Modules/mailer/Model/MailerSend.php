5aaa8030b56bdec8d073a9a00becdf94986976ba
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-07-24 16:53:42 +0200

    untabify to fix the mix of spaces and tabs

--- Modules/mailer/Model/MailerSend.php
+++ Modules/mailer/Model/MailerSend.php
@@ -11,7 +11,7 @@ require("externals/PHPMailer/PHPMailerAutoload.php");
  */
 class MailerSend extends Model {
 
-	public function sendEmailSimulate($from, $fromName, $toAdress, $subject, $content, $sentCopyToFrom = true){
+    public function sendEmailSimulate($from, $fromName, $toAdress, $subject, $content, $sentCopyToFrom = true){
             echo "send email from " .$fromName. "(" . $from . ") to ";
             print_r($toAdress);
             echo " subject = " . $subject . " content = " . $content. "<br/>";
@@ -20,62 +20,62 @@ class MailerSend extends Model {
             }
         }
 
-	public function sendEmail($from, $fromName, $toAdress, $subject, $content, $sentCopyToFrom = true ){
+    public function sendEmail($from, $fromName, $toAdress, $subject, $content, $sentCopyToFrom = true ){
 
-		// send the email
-		$mail = new PHPMailer();
-		$mail->IsHTML(true);
-		$smtp_host = getenv('SMTP_HOST');
+        // send the email
+        $mail = new PHPMailer();
+        $mail->IsHTML(true);
+        $smtp_host = getenv('SMTP_HOST');
         $smtp_port = 25;
         if(getenv('SMTP_PORT')) {
             $smtp_port= intval(getenv('SMTP_PORT'));
         }
-		if (!empty($smtp_host)) {
-			$mail->isSMTP();
-			$mail->Host = $smtp_host;
+        if (!empty($smtp_host)) {
+            $mail->isSMTP();
+            $mail->Host = $smtp_host;
             $mail->Port = $smtp_port;
-		}
-		$mail->CharSet = "utf-8";
-		$mail->SetFrom($from, $fromName);
-		$mail->Subject = $subject;
+        }
+        $mail->CharSet = "utf-8";
+        $mail->SetFrom($from, $fromName);
+        $mail->Subject = $subject;
 
-		// parse content
-		$content = preg_replace("/\r\n|\r/", "<br />", $content);
-		$content = trim($content);
+        // parse content
+        $content = preg_replace("/\r\n|\r/", "<br />", $content);
+        $content = trim($content);
 
-		$mail->Body = $content;
+        $mail->Body = $content;
 
-		if ($sentCopyToFrom){
-			$mail->AddCC($from);
-		}
+        if ($sentCopyToFrom){
+            $mail->AddCC($from);
+        }
 
-		if (is_array ($toAdress)){
-			foreach($toAdress as $addres){
-				if ($addres[0] && $addres[0] != ""){
+        if (is_array ($toAdress)){
+            foreach($toAdress as $addres){
+                if ($addres[0] && $addres[0] != ""){
                                     //echo $addres[0] . "<br/>";
                                     //$mail->AddAddress($addres[0]);
                                     $mail->addBCC($addres[0]);
-				}
-			}
-		}
-		else{
-			if ( $toAdress != "" ){
-				//$mail->AddAddress($toAdress);
-				$mail->addBCC($toAdress);
-			}
-		}
+                }
+            }
+        }
+        else{
+            if ( $toAdress != "" ){
+                //$mail->AddAddress($toAdress);
+                $mail->addBCC($toAdress);
+            }
+        }
 
-		// get the language
-		$lang = "En";
-		if (isset ( $_SESSION ["user_settings"] ["language"] )) {
-			$lang = $_SESSION ["user_settings"] ["language"];
-		}
+        // get the language
+        $lang = "En";
+        if (isset ( $_SESSION ["user_settings"] ["language"] )) {
+            $lang = $_SESSION ["user_settings"] ["language"];
+        }
 
-		if(!$mail->Send()) {
-			return MailerTranslator::Message_Not_Send($lang) . $mail->ErrorInfo;
-		} else {
-			return MailerTranslator::Message_Send($lang);
-		}
-	}
+        if(!$mail->Send()) {
+            return MailerTranslator::Message_Not_Send($lang) . $mail->ErrorInfo;
+        } else {
+            return MailerTranslator::Message_Send($lang);
+        }
+    }
 
 }
