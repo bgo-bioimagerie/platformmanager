4e014043bd5bbe67aad9607a6cded7cc1da180bc
Olivier Sallou <olivier.sallou@irisa.fr>
2021-06-11 16:00:28 +0200

    add missing code

--- Modules/users/View/Useraccount/indexAction.php
+++ Modules/users/View/Useraccount/indexAction.php
@@ -33,7 +33,38 @@
         </div>
         
         <?php echo $formHtml ?>
+
+
+        <div class="col-md-12">
+            <h2>External connection providers</h2>
+        <?php
+        foreach ($providers as $provider) {
+        ?>
+            <a href="<?php echo $provider['login']; ?>?client_id=<?php echo $provider['client_id']; ?>&response_type=code&scope=openid&redirect_uri=<?php echo $provider['callback']; ?>">
+                <button type="button" class="btn btn-primary"><?php echo $provider['name']; ?></button>
+            </a>
+        <?php
+        }
+        ?>
+        </div>
+        <div class="col-md-12">
+            <h2>Linked providers</h2>
+            <table class="table" aria-label="external providers linked to account">
+            <thead><tr><th scope="col">Provider</th><th scope="col">ID</th></tr></thead>
+        <?php
+        foreach ($linked as $link) {
+        ?>
+            <thead><tr><td><?php echo $link['provider'];?></td><td><?php echo $link['oid'];?></td></tr></thead>
+        <?php
+        }
+        ?>
+            </table>
+        </div>
+
+
+
     </div>
+    
 </div> <!-- /container -->
 <?php
 endblock();
