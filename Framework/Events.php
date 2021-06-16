2c1c309630c9bc311d656c5bbab58e3ad5f1ee48
Olivier Sallou <olivier.sallou@irisa.fr>
2021-06-16 10:47:37 +0200

    Add Events handler to send/manage pfm events (#165)
    * add Events class to send events to rabbitmq exchange (if configured), add events for space creation/deletion and user join/unjoin
    
    * add events handler service bin/pfm-events.php

---
+++ Framework/Events.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once 'Framework/Configuration.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class Events {
+
+    public const ACTION_SPACE_CREATE = 0;
+    public const ACTION_SPACE_DELETE = 1;
+    public const ACTION_SPACE_USER_JOIN = 2;
+    public const ACTION_SPACE_USER_UNJOIN = 3; 
+
+    private static $connection;
+    private static $channel;
+
+    /**
+     * Initialize client
+     */
+    public static function getChannel() {
+        if(self::$channel === null) {
+            $amqpHost = Configuration::get('amqp_host', '');
+            if ($amqpHost == '') {
+                return null;
+            }
+            $amqpPort = Configuration::get('amqp_port', 5672);
+            $amqpUser = Configuration::get('amqp_user');
+            $amqpPassword = Configuration::get('amqp_password');
+            self::$connection = new AMQPStreamConnection($amqpHost, intval($amqpPort), $amqpUser, $amqpPassword);
+            self::$channel = self::$connection->channel();
+            self::$channel->exchange_declare('pfm_events', 'fanout', false, false, false);
+        }
+        return self::$channel;
+    }
+
+    /**
+     * Close connection
+     */
+    public static function Close() {
+        if(self::$channel != null) {
+            self::$channel->close();
+            self::$connection->close();
+        }
+    }
+        
+
+    /**
+     * Sends a message to rabbitmq
+     * @param array $message message to send
+     */
+    public static function send(array $message) {
+        try {
+            $channel = self::getChannel();
+            if($channel === null) {
+                return;
+            }
+            Configuration::getLogger()->debug('[event] send', ['message' => $message]);
+            $amqpMsg = new AMQPMessage(json_encode($message));
+            $channel->basic_publish($amqpMsg, 'pfm_events', '');
+        } catch (Exception $e) {
+            Configuration::getLogger()->error('[event] error', ['message' => $e->getMessage()]);
+            return;
+        }
+    }
+}
+
+?>
