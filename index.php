4e29d983dcec70cf5290d9f5951d1ea3b689998a
Olivier Sallou <olivier.sallou@irisa.fr>
2021-06-17 16:24:19 +0200

    add sentry init

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+require_once 'Framework/Configuration.php';
 // settings
 ini_set('display_errors', 0); 
 //error_reporting(E_ALL); 
@@ -9,6 +10,11 @@ $sessionPath = dirname(__FILE__).'/tmp/';
 session_save_path( $sessionPath );
 
 require 'Framework/Router.php';
+
+if(Configuration::get('sentry_dsn', '')) {
+    \Sentry\init(['dsn' => Configuration::get('sentry_dsn')]);
+}
+
 
 $router = new Router();
 $router->routerRequest();
