738dfc520f6eb1270886d01dc036b0ec9273b637
Olivier Sallou <olivier.sallou@irisa.fr>
2021-08-16 11:00:04 +0200

    fix antibodies route and history route ACL Closes #258

--- Modules/antibodies/AntibodiesRouting.php
+++ Modules/antibodies/AntibodiesRouting.php
@@ -11,7 +11,7 @@ class AntibodiesRouting extends Routing{
         $this->addRoute("antibodiesconfig", "antibodiesconfig", "antibodiesconfig", "index", array("id_space"), array(""));
 
         // add here the module routes
-        $this->addRoute("antibodies", "antibodies", "antibodieslist", "index", array("id_space", "letter"), array("", ""));
+        $this->addRoute("antibodies", "antibodies", "antibodieslist", "index", array("id_space", "sortentry"), array("", ""));
         $this->addRoute("apiantibodytissus", "apiantibodytissus", "antibodies", "tissus", array("id_space", "id_tissus"), array("", ""),true);
         $this->addRoute("deletetissus", 'deletetissus', 'antibodieslist', "deletetissus", array('id_space', 'id_tissus'), array("", ""));
         $this->addRoute("deleteowner", 'deleteowner', 'antibodieslist', "deleteowner", array('id_space', 'id_owner'), array("", ""));
