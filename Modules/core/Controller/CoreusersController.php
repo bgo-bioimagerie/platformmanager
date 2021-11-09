d05798a6d4c2fa1bd25b1159cc58080987b94838
Gaetan Herve <x-GHerve>
2021-11-09 13:12:38 +0100

    stop checking input values at first invalid value

--- Modules/core/Controller/CoreusersController.php
+++ Modules/core/Controller/CoreusersController.php
@@ -128,29 +128,29 @@ class CoreusersController extends CoresecureController {
 
             if(!$form->getParameter("email") || !$modelUser->isEmailFormat($form->getParameter("email"))) {
                 $canEditUser = false;
-                $_SESSION["flash"] = CoreTranslator::EmailInvalid($lang);
-                $_SESSION["flashClass"] = "danger";
+                $this->displayFormWarnings("EmailInvalid", $id, $lang);
+                return;
             }
             if (!$id) {
                 // creating a new user
                 if ($modelUser->isLogin($this->request->getParameter('login'))) {
                     $canEditUser = false;
-                    $_SESSION["flash"] = CoreTranslator::LoginAlreadyExists($lang);
-                    $_SESSION["flashClass"] = "danger";
+                    $this->displayFormWarnings("LoginAlreadyExists", $id, $lang);
+                    return;
                 }
                 if($modelUser->isEmail($form->getParameter("email"))) {
                     // if email already exists, warn user
                     $canEditUser = false;
-                    $_SESSION["flash"] = CoreTranslator::EmailAlreadyExists($lang);
-                    $_SESSION["flashClass"] = "danger";
+                    $this->displayFormWarnings("EmailAlreadyExists", $id, $lang);
+                    return;
                 }
             } else {
                 // updating an existing user
                 if ($modelUser->isEmail($form->getParameter("email")) && ($form->getParameter("email") != $user["email"])) {
                     // if email, excepting user's one, already exists, warn user
                     $canEditUser = false;
-                    $_SESSION["flash"] = CoreTranslator::EmailAlreadyExists($lang);
-                    $_SESSION["flashClass"] = "danger";
+                    $this->displayFormWarnings("EmailAlreadyExists", $id, $lang);
+                    return;
                 }
             }
 
@@ -178,6 +178,12 @@ class CoreusersController extends CoresecureController {
         }
 
         $this->render(array("formHtml" => $form->getHtml($lang), "formPwdHtml" => $formPwdHtml, "script" => $script));
+    }
+
+    protected function displayFormWarnings($cause, $id, $lang) {
+        $_SESSION["flash"] = CoreTranslator::$cause($lang);
+        $_SESSION["flashClass"] = "danger";
+        $this->redirect("coreusersedit/" . $id ?? 0);
     }
 
     protected function editPwdQuery($formPwd, $modelUser, $lang) {
