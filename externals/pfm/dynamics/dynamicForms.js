e96be74415f571e663a602104a1992b1502643a9
Gaetan Herve <x-GHerve>
2021-11-30 17:24:38 +0100

    add clone mechanics to dynamicFields()

--- externals/pfm/dynamics/dynamicForms.js
+++ externals/pfm/dynamics/dynamicForms.js
@@ -4,10 +4,14 @@
  */
 export class DynamicForms {
 
+    selectBuffer = {
+        targetElement: []
+    };
+
     /**
-     * Generates content of an html input depending on an html select selected option  
-     * @param {string} field1 id of the source select
-     * @param {string} field2 id of the dependant input
+     * Generates content of one or several html inputs depending on an html select selected option  
+     * @param {HTMLElement} source id of the source select
+     * @param {[HTMLElements]} targets ids of the dependant inputs
      * @param {string} apiRoute
      * name of route to call the backend function returning data to fill dependant select options
      *  
@@ -22,28 +26,40 @@ export class DynamicForms {
             body: null
         };
         source.addEventListener("change", (event) => {
-            targets.forEach(target => {
-                let targetElement = target.element;
-                let apiRoute = target.apiRoute;
-                apiRoute += spaceId;
+            targets.forEach((target, index) => {
+                // let targetElement = target.element;
                 let id = event.target.value;
-                apiRoute += ("/" + id);
+                let apiRoute = target.apiRoute;
+                apiRoute += spaceId + "/" + id;
+
                 fetch(apiRoute, cfg, true).
                     then((response) => response.json()).
                     then(data => {
-                        switch (targetElement.nodeName) {
+                        switch (target.element.nodeName) {
                             case "SELECT":
+                                /**
+                                 * Since targetElement changes when cloning,
+                                 * need to buffer the new one.
+                                 * Indexed in case of if targets contain multiple selects
+                                 **/
+                                let targetElement = this.selectBuffer.targetElement[index] ?? target.element;
+                                // get elements to insert as options
                                 let elements = Array.isArray(data.elements) ? data.elements : [data.elements];
-                                targetElement.options.length = 0;
-                                elements.forEach( (element, index) => {
-                                    targetElement.options[index] = new Option(element.name, element.id);
+                                // clone target
+                                let cloneElement = targetElement.cloneNode(false);
+                                elements.forEach( (element) => {
+                                    cloneElement.appendChild(new Option(element.name, element.id));
                                 });
+                                // replacing target element by its clone
+                                targetElement.replaceWith(cloneElement);
+                                // buffering clone element to use it as the new target element
+                                this.selectBuffer.targetElement[index] = document.getElementById(targetElement.id);
                                 break;
                             case "#Text":
-                                targetElement.value = data.elements;
+                                target.element.value = data.elements;
                                 break;
                             case "TEXTAREA":
-                                targetElement.value = data.elements;
+                                target.element.value = data.elements;
                                 break;
                             default:
                                 break;
