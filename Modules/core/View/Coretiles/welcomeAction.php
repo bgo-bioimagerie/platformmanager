64a40eca32e964223ef08289fc4a252c9b57d302
Olivier Sallou <olivier.sallou@irisa.fr>
2021-12-01 09:49:33 +0100

    [welcome] when logged show user future bookings and opened projects

--- Modules/core/View/Coretiles/welcomeAction.php
+++ Modules/core/View/Coretiles/welcomeAction.php
@@ -10,6 +10,7 @@
         width:220px; 
         margin-left: 25px;
         margin-top: 25px;
+        background-color: white;
     }
 
 </style>
@@ -18,11 +19,11 @@
 <!-- body -->     
 <?php startblock('content') ?>
 
+<div class="container" id="welcome">
+    <div class="row">
+        <div class="col-md-8">
 
-<div class="pm-tile-container">
-    <div >
-        <div id="spacesearch" class="col-xs-12" style="margin: 50px;">
-            <div class="row">
+            <div class="row" style="margin: 10px;">
                 <div class="col-xs-6">
                     <input id="search" type="form-control" v-model="search" placeholder="search"/>
                 </div>
@@ -52,39 +53,67 @@
                     </div>
                 </div>
             </div>
+            <div class="row" id="user_stars">
+                <?php foreach($spaces as $item) { ?>
+                <div class="col-xs-12 col-md-4 col-lg-2 modulebox">
+                    <a href="<?php echo "corespace/" . $item["id"] ?>">
+                    <?php if(isset($icon)) {?><img aria-label="space logo" onerror="this.style.display='none'" src="<?php echo $item["image"] ?>" alt="logo" style="margin-left: -15px;width:218px;height:150px"><?php } ?>
+                    </a>
+                    <p></p>
+                    <p style="color:#018181; ">
+                        <a href="<?php echo "corespace/" . $item["id"] ?>"> <?php echo $item["name"] ?></a>
+                        <?php if(isset($_SESSION["login"])) { ?>
+                                <a aria-label="remove from favorites" href="<?php echo "coretiles/1/0/unstar/".$item["id"] ?>"><span aria-hidden="true" class="glyphicon glyphicon-star"></span></a>
+                        <?php } ?>
+                        <?php if($item["status"] == 0) { echo '<span class="glyphicon glyphicon-lock" aria-hidden="true" aria-label="private"></span>'; } ?>
+                    </p>
+                    <p style="color:#a1a1a1; font-size:12px;">
+                        <?php echo $item["description"] ?>
+                    </p>
+                    <div style="position: absolute; bottom: 0px">
+                        <small>
+                        <?php if($item["support"]) {  echo 'support: <a href="mailto:'.$item["support"].'">'.$item["support"].'</a>'; } ?>
+                        </small>
+                    </div>
+                </div>  
+                <?php } ?>
+            </div>
+            <?php if(!isset($_SESSION['id_user']) || !$_SESSION['id_user']) { ?>
             <div class="row">
                 <div class="col-xs-12">
                     <?php echo $content; ?>
                 </div>
             </div>
-
-        <?php foreach($spaces as $item) { ?>
-
-            <div class="col-xs-12 col-md-4 col-lg-2 modulebox">
-                <a href="<?php echo "corespace/" . $item["id"] ?>">
-                <?php if(isset($icon)) {?><img aria-label="space logo" onerror="this.style.display='none'" src="<?php echo $item["image"] ?>" alt="logo" style="margin-left: -15px;width:218px;height:150px"><?php } ?>
-                </a>
-                <p></p>
-                <p style="color:#018181; ">
-                    <a href="<?php echo "corespace/" . $item["id"] ?>"> <?php echo $item["name"] ?></a>
-                    <?php if(isset($_SESSION["login"])) { ?>
-                            <a aria-label="remove from favorites" href="<?php echo "coretiles/1/0/unstar/".$item["id"] ?>"><span aria-hidden="true" class="glyphicon glyphicon-star"></span></a>
-                    <?php } ?>
-                    <?php if($item["status"] == 0) { echo '<span class="glyphicon glyphicon-lock" aria-hidden="true" aria-label="private"></span>'; } ?>
-                </p>
-                <p style="color:#a1a1a1; font-size:12px;">
-                    <?php echo $item["description"] ?>
-                </p>
-                <div style="position: absolute; bottom: 0px">
-                    <small>
-                    <?php if($item["support"]) {  echo 'support: <a href="mailto:'.$item["support"].'">'.$item["support"].'</a>'; } ?>
-                    </small>
+            <?php } ?>
+        </div>
+        <div class="col-md-4" id="user_home">
+            <?php if(isset($_SESSION['id_user']) && $_SESSION['id_user']) { ?>
+                <div v-if="bookings" id="future_bookings">
+                    <div class="panel panel-default">
+                    <div class="panel-heading">Bookings</div>
+                    <div class="panel-body">
+                        <div class="row" v-for="b in bookings">
+                            <div class="col-xs-12">
+                                {{b.date}}: {{b.resource}} [{{b.space}}] <a :href="`/bookingeditreservation/${b.id_space}/r_${b.id}`"><span class="glyphicon glyphicon-zoom-in"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
-            </div>  
 
-
-
-        <?php } ?>
+                <div v-if="project" id="projects">
+                    <div class="panel panel-default">
+                    <div class="panel-heading">Service projects</div>
+                    <div class="panel-body">
+                        <div class="row" v-for="b in projects">
+                            <div class="col-xs-12">
+                                {{b.date}}: {{b.name}} [{{b.space}}] <a :href="`/servicesprojectsheet/${b.id_space}/${b.id}`"><span class="glyphicon glyphicon-zoom-in"></span></a>
+                            </div>
+                        </div>
+                </div>
+            </div>
+            </div>
+            <?php } ?>
         </div>
     </div>
 </div> <!-- /container -->
@@ -94,14 +123,71 @@ let resources = <?php echo json_encode($resources); ?>;
 let catalog = <?php echo json_encode($catalog); ?>;
 
 var app = new Vue({
-    el: '#spacesearch',
+    el: '#welcome',
     data () {
         return {
+            logged: <?php if(isset($_SESSION['id_user']) && $_SESSION['id_user']) { echo "true"; } else { echo "false";} ?>,
             spaces: <?php echo json_encode($spaceMap); ?> ,
             catalog: <?php echo json_encode($resources); ?>,
             resources: <?php echo json_encode($catalog); ?>,
             search: '',
-            matches: []
+            matches: [],
+            bookings: [],
+            projects: [],
+        }
+    },
+    mounted: function() {
+        if(!this.logged) {
+            return
+        }
+        let headers = new Headers();
+                headers.append('Content-Type','application/json');
+                headers.append('Accept', 'application/json');
+        let cfg = {
+            headers: headers,
+            method: 'GET',
+        };
+        try {
+            fetch(`/user/booking/future/0/0`, cfg).
+                then((response) => response.json()).
+                then(data => {
+                    let bookings = []
+                    data.bookings.forEach((elem) => {
+                        let bdate = new Date(elem.start_time*1000);
+                        bookings.push({
+                            "id": elem.id,
+                            "id_space": elem.id_space,
+                            "resource": elem.resource,
+                            "id_resource": elem.resource_id,
+                            "space": elem.space,
+                            "date": `${bdate.toLocaleDateString()} ${bdate.toLocaleTimeString()}`
+                        });
+                    });
+
+                    this.bookings = bookings
+                })
+        } catch(error) {
+            console.debug('failed to get user bookings', error);
+        }
+        try {
+            fetch(`/user/services/projects/0`, cfg).
+                then((response) => response.json()).
+                then(data => {
+                    let projects = []
+                    data.projects.forEach((elem) => {
+                        projects.push({
+                            "id": elem.id,
+                            "id_space": elem.id_space,
+                            "name": elem.name,
+                            "space": elem.space,
+                            "date": elem.date_open
+                        });
+                    });
+
+                    this.projects = projects
+                })
+        } catch(error) {
+            console.debug('failed to get user bookings', error);
         }
     },
     watch: {
