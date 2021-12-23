dda8e16a6a712011eadb22401d5cd382d4677d40
Olivier Sallou <olivier.sallou@irisa.fr>
2021-12-23 11:25:46 +0100

    [test] add booking activation

---
+++ tests/manager/BookingTest.php
@@ -0,0 +1,69 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+require './vendor/autoload.php';
+require_once 'Modules/core/Controller/CoreconnectionController.php';
+require_once 'Framework/Request.php';
+require_once 'Framework/Configuration.php';
+
+require_once 'Modules/booking/Controller/BookingconfigController.php';
+
+
+require_once 'tests/BaseTest.php';
+
+
+class BookingTest extends BaseTest {
+
+    private function setupBooking($space, $user) {
+        Configuration::getLogger()->debug('setup booking', ['user' => $user, 'space' => $space]);
+        $this->asUser($user['login'], $space['id']);
+        // activate booking module
+        $req = new Request([
+            "path" => "bookingconfig/".$space['id'],
+            "formid" => "menusactivationForm",
+            "bookingmenustatus" => 2,
+            "displayBookingMenu" => '',
+            "colorBookingMenu" =>  "#000000",
+            "colorTxtBookingMenu" => "#ffffff",
+            "bookingsettingsmenustatus" => 3,
+            "displaySettingsMenu" => '',
+            "colorSettingsMenu" =>  "#000000",
+            "colorTxtSettingsMenu" => "#ffffff"
+
+        ], false);
+        $c = new BookingconfigController($req);
+        $c->indexAction($space['id']);
+        $c = new CorespaceController(new Request(["path" => "corespace/".$space['id']], false));
+        $spaceView = $c->viewAction($space['id']);
+        $clientsEnabled = false;
+        foreach($spaceView['spaceMenuItems'] as $menu) {
+            if($menu['url'] == 'booking') {
+                $clientsEnabled = true;
+            }
+        }
+        $this->assertTrue($clientsEnabled);
+        $clientsEnabled = false;
+        foreach($spaceView['spaceMenuItems'] as $menu) {
+            if($menu['url'] == 'bookingsettings') {
+                $clientsEnabled = true;
+            }
+        }
+        $this->assertTrue($clientsEnabled);
+    }
+
+    public function testConfigureModuleBooking() {
+        $ctx = $this->Context();
+        $spaces = $ctx['spaces'];
+        foreach($spaces as $spaceName => $data) {
+            $space = $this->space($spaceName);
+            if(empty($data['admins'])) {
+                continue;
+            }
+            $user = $this->user($data['admins'][0]);
+            $this->setupBooking($space, $user);
+        }
+    }
+
+}
+
+?>
