9d86faecfedf2a9056ebb2b2c68bafde05260e0f
Olivier Sallou <olivier.sallou@irisa.fr>
2022-01-31 14:06:13 +0100

    add mailer edit rights

--- db/upgrade/1642743110_add_mailer_table.php
+++ db/upgrade/1642743110_add_mailer_table.php
@@ -2,6 +2,8 @@
 require_once 'Framework/Model.php';
 require_once 'Framework/Configuration.php';
 require_once 'Modules/mailer/Model/Mailer.php';
+require_once 'Modules/core/Model/CoreSpace.php';
+require_once 'Modules/core/Model/CoreConfig.php';
 
 # Upgrade: add mailer table
 class CoreUpgradeDB1642743110 extends Model {
@@ -9,6 +11,19 @@ class CoreUpgradeDB1642743110 extends Model {
 	  Configuration::getLogger()->info("[db][upgrade] Apply add mailer table");
 	  $m = new Mailer();
 	  $m->createTable();
+
+
+	  $modelCoreConfig = new CoreConfig();
+	  $cp = new CoreSpace();
+	  $spaces = $cp->getSpaces('id');
+	  foreach ($spaces as $space) {
+		$id_space = $space['id'];
+		$mailerEdit = $modelCoreConfig->getParamSpace('mailerEdit', $id_space, '');
+		if(!$mailerEdit) {
+		  $modelCoreConfig->setParam('mailerEdit', CoreSpace::$ADMIN, $id_space);
+		}
+	  }
+
   }
 }
 $db = new CoreUpgradeDB1642743110();
