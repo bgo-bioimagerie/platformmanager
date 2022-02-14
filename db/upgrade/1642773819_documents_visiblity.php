08ff3a811860f4015faa05517592864aadffb82b
Olivier Sallou <olivier.sallou@irisa.fr>
2022-02-14 10:21:27 +0100

    fix spaces/tabs [skip ci]

--- db/upgrade/1642773819_documents_visiblity.php
+++ db/upgrade/1642773819_documents_visiblity.php
@@ -7,13 +7,12 @@ require_once 'Modules/core/Model/CoreSpace.php';
 # Upgrade: documents_visiblity
 class CoreUpgradeDB1642773819 extends Model {
   public function run(){
-	  Configuration::getLogger()->info("[db][upgrade] Apply documents_visibility");
-	  $sql = "ALTER TABLE dc_documents ADD COLUMN visibility INT NOT NULL DEFAULT 0";
+    Configuration::getLogger()->info("[db][upgrade] Apply documents_visibility");
+    $sql = "ALTER TABLE dc_documents ADD COLUMN visibility INT NOT NULL DEFAULT 0";
     $this->runRequest($sql);
     $sql = "ALTER TABLE dc_documents ADD COLUMN id_ref INT NOT NULL DEFAULT 0";
     $this->runRequest($sql);
     Configuration::getLogger()->info("[db][upgrade] Apply documents_visibility, done!");
-
 
     $modelCoreConfig = new CoreConfig();
     $cp = new CoreSpace();
