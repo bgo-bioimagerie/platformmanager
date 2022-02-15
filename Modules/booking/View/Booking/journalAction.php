261353a90e3ab64a3484b54160a5f7fe5d9107d2
Olivier Sallou <olivier.sallou@irisa.fr>
2022-02-15 17:08:56 +0100

    bs5

--- Modules/booking/View/Booking/journalAction.php
+++ Modules/booking/View/Booking/journalAction.php
@@ -3,7 +3,7 @@
     
 <?php startblock('content') ?>
 
-<div class="container">
+<div class="container pm-table">
     <h3>Last 30 days and upcoming bookings</h3>
     <table class="table" aria-label="last 30 days and future bookings">
     <thead><tr><th scope="col">Id</th><th scope="col">Start time</th><th scope="col">Resource</th><th scope="col"></th></tr></thead>
--- Modules/documents/View/Documentslist/indexAction.php
+++ Modules/documents/View/Documentslist/indexAction.php
@@ -2,7 +2,7 @@
 
 
 <?php startblock('content') ?>
-<div class="container">
+<div class="container pm-table">
     <div class="row">
         <div class="col-12" id="doctree">
 
