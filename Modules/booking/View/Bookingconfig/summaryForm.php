3a63529ae4301a2724fc5d23c3d9535b0bb364f0
Olivier Sallou <olivier.sallou@irisa.fr>
2022-03-03 18:35:56 +0100

    fix booking summary layout for bs5

--- Modules/booking/View/Bookingconfig/summaryForm.php
+++ Modules/booking/View/Bookingconfig/summaryForm.php
@@ -1,4 +1,4 @@
-<div class="col-12 col-10 offset-1 pm-form-short" style="background-color: #fff; border-radius: 7px; padding: 7px;">
+<div class="col-12 pm-form-short" style="background-color: #fff; border-radius: 7px; padding: 7px;">
     <div class="page-header">
         <h3>
             <?php echo BookingTranslator::Booking_summary_options($lang) ?> <br> <small></small>
@@ -21,8 +21,7 @@
         ?>
         <form role="form" class="form-horizontal" action="bookingconfig/<?php echo $id_space ?>"
               method="post">
-
-            <div class="col-10">
+            <div>
                 <input class="form-control" type="hidden" name="setbookingoptionsquery" value="yes"
                        />
             </div>
@@ -42,7 +41,7 @@
                 $tag_font = $this->clean($bookingSettings[$i]['font'] ?? 'normal');
             }
             ?>
-            <div class="col-12">
+            <div class="row">
                 <div class="col-3"><label class="control-label">Recipient name:</label></div>
                 <div class="col-2"><select class="form-control" name="tag_visible_rname">
                     <OPTION value="1" <?php if ($tag_visible == 1) {
@@ -104,7 +103,7 @@
         $tag_font = $this->clean($bookingSettings[$i]['font']);
     }
     ?>
-            <div class="col-12">
+            <div class="row">
                 <div class="col-3"><label class="control-label">Recipient phone:</label></div>
                 <div class="col-2"><select class="form-control" name="tag_visible_rphone">
                     <OPTION value="1" <?php if ($tag_visible == 1) {
@@ -166,7 +165,7 @@
             $tag_font = $this->clean($bookingSettings[$i]['font']);
         }
                         ?>
-            <div class="col-12">
+            <div class="row">
                 <div class="col-3"><label class="control-label">Short description:</label></div>
                 <div class="col-2"><select class="form-control" name="tag_visible_sdesc">
                     <OPTION value="1" <?php if ($tag_visible == 1) {
@@ -227,7 +226,7 @@
             $tag_font = $this->clean($bookingSettings[$i]['font']);
         }
         ?>
-            <div class="col-12">
+            <div class="row">
                 <div class="col-3"><label class="control-label">Description:</label></div>
                 <div class="col-2"><select class="form-control" name="tag_visible_desc">
                     <OPTION value="1" <?php if ($tag_visible == 1) {
@@ -273,7 +272,7 @@
     } ?>> italic </OPTION>
                     </select></div>
                 <br></br>
-                <div class="col-2 offset-10" id="button-div">
+                <div class="col-2 offset-1" id="button-div">
                     <input type="submit" class="btn btn-primary" value="save" />
                 </div>
         </form>
