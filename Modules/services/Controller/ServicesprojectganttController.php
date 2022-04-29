3d635abe2064b839632fda813c62fdf006650642
Gaetan Herve <x-GHerve>
2022-04-29 15:19:48 +0200

    clean

--- Modules/services/Controller/ServicesprojectganttController.php
+++ Modules/services/Controller/ServicesprojectganttController.php
@@ -24,11 +24,8 @@ class ServicesprojectganttController extends ServicesController {
      * @see Controller::indexAction()
      */
 
-     /* TODO: gérer les autorisations par projet ?
-     * ajouter visu sur tâches au click sur projet
-     * => ajouter des dates aux tâches
-     *
-    */
+     // TODO: gérer les autorisations par projet ?
+
     public function indexAction($id_space, $allPeriod = 0, $incharge = "") {
 
         $this->checkAuthorizationMenuSpace("services", $id_space, $_SESSION["id_user"]);
@@ -55,7 +52,7 @@ class ServicesprojectganttController extends ServicesController {
             if( $projectperiodbeginArray[1] <= date("m", time()) ){
                 $year = date("Y", time());
             }
-            else{
+            else {
                 $year = date("Y", time()) - 1;
             }
             $yearp = $year + 1;
@@ -67,99 +64,21 @@ class ServicesprojectganttController extends ServicesController {
             } else {
                 $projects = $modelProject->allPeriodProjectsByInCharge($id_space, $incharge, $periodStart, $periodEnd);
             }
-        }
-        else{
+        } else {
             if ($incharge == "") {
                 $projects = $modelProject->allOpenedProjects($id_space);
             } else {
                 $projects = $modelProject->allOpenedProjectsByInCharge($id_space, $incharge);
             }
         }
-
-        // format into json
-        $modelUser = new CoreUser();
-        $projectsjson = "[";        
-        $first = true;
-        $modelClient = new ClClient();
-        $modelBelonging = new ClPricing();
-        foreach ($projects as $proj) {
-            $belInfo = $modelClient->get($id_space, $proj["id_resp"]);
-            
-            $bkColor = 'col' . $belInfo["id"];
-            
-            if (!$first) {
-                $projectsjson .= ",";
-            }
-            $first = false;
-            $projectsjson .= "{";
-
-            $visa = $modelVisa->get($id_space ,$proj["in_charge"]);
-            $projectsjson .= "name: \"" . $modelUser->getUserInitiales($visa["id_user"]) . "\",";
-            $projectsjson .= "desc: \"" . $proj["name"] . "\",";
-            $projectsjson .= "values: [{";
-
-            $startTime = time();
-            if ($proj["date_open"] && $proj["date_open"] != "0000-00-00") {
-                $startTime = strtotime($proj["date_open"]);
-            }
-
-            $projectsjson .= "from: \"/Date(" . 1000 * $startTime . ")/\",";
-
-            $dateEnd = time();
-            if ($proj["time_limit"] && $proj["time_limit"] != "0000-00-00" && $proj["time_limit"] != "") {
-                $dateEnd = strtotime($proj["time_limit"]);
-            }
-
-            $projectsjson .= "to: \"/Date(" . 1000 * $dateEnd . ")/\",";
-            $projectsjson .= "label: \"" . $proj["name"] . "\",";
-            $projectsjson .= "customClass: \"" . $bkColor . "\"";
-            $projectsjson .= "}";
-
-            $closeTime = 0;
-            if ($proj["date_close"] && $proj["date_close"] != "0000-00-00" && $proj["date_close"] != "") {
-                $closeTime = strtotime($proj["date_close"]);
-            }
-            if ($closeTime == 0) {
-                $closeTime = time();
-            }
-            if ($closeTime > $dateEnd) {
-                $projectsjson .= ",{";
-                $projectsjson .= "from: \"/Date(" . 1000 * $dateEnd . ")/\",";
-                $projectsjson .= "to: \"/Date(" . 1000 * $closeTime . ")/\",";
-                $projectsjson .= "label: \"" . "\",";
-                $projectsjson .= "customClass: \"colTimeOver\"";
-                $projectsjson .= "}";
-            }
-
-            $projectsjson .= "]";
-            $projectsjson .= "}";
-        }
-        $projectsjson .= "]";
-
-
-        $bels = $modelBelonging->getAll($id_space);
-        $css = "";
-        foreach ($bels as $bel) {
-
-            $css .= ".fn-gantt ." . "col" . $bel["id"] . " {";
-            $css .= "background-color: " . $bel["color"] . ";";
-            $css .= "}";
-        }
-        $css .= ".fn-gantt ." . "colTimeOver" ." {";
-        $css .= "background-color: #DFAF2C;";
-        $css .= "}";
-
-
         $personInCharge = $modelVisa->getAll($id_space);
         
         $data = array(
             'lang' => $lang,
             'id_space' => $id_space,
             'projects' => json_encode($projects),
-            'projectsjson' => $projectsjson,
             'personInCharge' => $personInCharge,
             'activeGantt' => $incharge,
-            'css' => $css,
             'allPeriod' => $allPeriod
         );
         // render
