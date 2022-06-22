f4b94e3ce0e39b0e26916a12e906bbb36a41a031
Olivier Sallou <olivier.sallou@irisa.fr>
2022-06-22 16:28:56 +0200

    add rating install on upgrade

---
+++ db/upgrade/1655908007_add_rating_install.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'Framework/Model.php';
+require_once 'Framework/Configuration.php';
+
+require_once 'Modules/rating/Model/RatingInstall.php';
+
+# Upgrade: add rating install
+class CoreUpgradeDB1655908007 extends Model {
+  public function run(){
+    Configuration::getLogger()->info("[db][upgrade] Apply add rating install");
+    $r = new RatingInstall();
+    $r->createTable();
+    Configuration::getLogger()->info("[db][upgrade] Apply add rating install, done!");
+  }
+}
+$db = new CoreUpgradeDB1655908007();
+$db->run();
+?>
