1c362d79ff4aaf5ac7a23f361e3eaf3c79e7953c
Olivier Sallou <olivier.sallou@irisa.fr>
2022-06-24 06:49:00 +0200

    [core][db][cli] upgrade fixes from old versions

--- db/upgrade/1651744382_autoincr_acowner.php
+++ db/upgrade/1651744382_autoincr_acowner.php
@@ -5,14 +5,15 @@ require_once 'Framework/Configuration.php';
 class CoreUpgradeDB1651744382 extends Model {
   public function run(){
 	  Configuration::getLogger()->info("[db][upgrade] Apply autoincr acowner");
-	  try {
+	  if($this->checkColumn('ac_j_user_anticorps', 'id')) {
+		Configuration::getLogger()->info('[db][upgrade] ac_j_user_anticorps.id already exists, modifying it');
+		$sql = 'ALTER TABLE ac_j_user_anticorps MODIFY COLUMN id int(11) NOT NULL AUTO_INCREMENT';
+		$this->runRequest($sql);
+	  } else {
+		Configuration::getLogger()->info('[db][upgrade] ac_j_user_anticorps.id does not exists, adding it');
 		$sql = 'ALTER TABLE ac_j_user_anticorps ADD COLUMN id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT';
 		$this->runRequest($sql);
-	  } catch(Throwable) {
-		Configuration::getLogger()->warning("[db][upgrade] id column creation for ac_j_user_anticorps failed, this is fine as it may already exists");
 	  }
-	  $sql = 'ALTER TABLE ac_j_user_anticorps MODIFY COLUMN id int(11) NOT NULL AUTO_INCREMENT';
-	  $this->runRequest($sql);
 	  Configuration::getLogger()->info("[db][upgrade] Apply autoincr acowner done");
   }
 }
