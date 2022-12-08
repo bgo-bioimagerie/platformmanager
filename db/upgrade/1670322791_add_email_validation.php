a88177be0b52008772b54f701a02e82459a9abfa
Olivier Sallou <olivier.sallou@irisa.fr>
2022-12-08 15:34:39 +0100

    linting

--- db/upgrade/1670322791_add_email_validation.php
+++ db/upgrade/1670322791_add_email_validation.php
@@ -2,8 +2,9 @@
 require_once 'Framework/Model.php';
 require_once 'Framework/Configuration.php';
 # Upgrade: add email validation
-class CoreUpgradeDB1670322791 extends Model {
-  public function run(){
+class CoreUpgradeDB1670322791 extends Model
+{
+  public function run() {
     Configuration::getLogger()->info("[db][upgrade] Apply add email validation");
     $this->addColumn('core_users', 'date_email_expiration', "int", 0);
     $this->addColumn('core_users', 'nb_email_expiration', "int", 0);
